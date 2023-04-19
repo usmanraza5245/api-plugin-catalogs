087777ba4e7e0576faa7c8cf56a17506f7c4c006
=usmanraza5245 <usmanraza5245@gmail.com>
2023-04-19 14:42:46 +0500

    update: update catalog query by removing expired items

--- src/queries/catalogItems.js
+++ src/queries/catalogItems.js
@@ -13,57 +13,81 @@ import ReactionError from "@reactioncommerce/reaction-error";
  * @param {String[]} [params.tags] - Tag IDs to include (OR)
  * @returns {Promise<MongoCursor>} - A MongoDB cursor for the proper query
  */
-export default async function catalogItems(context, { searchQuery, shopIds, flag, tagIds, catalogBooleanFilters } = {}) {
+export default async function catalogItems(
+  context,
+  { searchQuery, shopIds, flag, tagIds, catalogBooleanFilters } = {}
+) {
   const { collections } = context;
   const { Catalog } = collections;
-
+  const today = new Date().toISOString().substring(0, 10);
   if ((!shopIds || shopIds.length === 0) && (!tagIds || tagIds.length === 0)) {
-    throw new ReactionError("invalid-param", "You must provide tagIds or shopIds or both");
+    throw new ReactionError(
+      "invalid-param",
+      "You must provide tagIds or shopIds or both"
+    );
   }
 
   // if we send filter option from frontend
-  if( flag ){
-    console.log("queries without flag", catalogBooleanFilters)
+  if (flag) {
+    console.log("queries without flag", catalogBooleanFilters);
     let query = {
       "product.isDeleted": { $ne: true },
       ...catalogBooleanFilters,
       "product.isVisible": true,
-      "product.metafields.value": flag ? flag : ''
+      "product.metafields.value": flag ? flag : "",
+      "product.metafields": {
+        $elemMatch: {
+          key: "dueDate",
+          value: { $gte: today },
+        },
+      },
+      // $and: [
+      //   { "product.metafields.key": "dueDate" },
+      //   { "product.metafields.value": { $gte: { $toDate: today } } },
+      // ],
     };
-  
+
     if (shopIds) query.shopId = { $in: shopIds };
     if (tagIds) query["product.tagIds"] = { $in: tagIds };
-  
+
     if (searchQuery) {
       query.$text = {
-        $search: _.escapeRegExp(seapmrchQuery)
+        $search: _.escapeRegExp(seapmrchQuery),
       };
     }
-    console.log("filter query", query)
-    let result = await Catalog.find(query).toArray()
-    console.log("result ->>>>>>>", result)
+    console.log("filter query", query);
+    // let result = await Catalog.find(query).toArray();
+    // console.log("result ->>>>>>>", result);
     return Catalog.find(query);
   } else {
     // if we dont send any filter option
-    console.log("queries without flag", catalogBooleanFilters)
-    console.log("if flaq is not sent")
+    console.log("queries without flag", catalogBooleanFilters);
+    console.log("if flaq is not sent");
     let query = {
       "product.isDeleted": { $ne: true },
       ...catalogBooleanFilters,
       "product.isVisible": true,
+      "product.metafields": {
+        $elemMatch: {
+          key: "dueDate",
+          value: { $gte: today },
+        },
+      },
     };
-  
+
     if (shopIds) query.shopId = { $in: shopIds };
     if (tagIds) query["product.tagIds"] = { $in: tagIds };
-  
+
     if (searchQuery) {
       query.$text = {
-        $search: _.escapeRegExp(seapmrchQuery)
+        $search: _.escapeRegExp(seapmrchQuery),
       };
     }
-    console.log("filter query", query)
-    let result = await Catalog.find(query).sort({"product.upVotes": -1}).toArray()
-    console.log("result ->>>>>>>", result)
-    return Catalog.find(query)
+    console.log("filter query", query);
+    let result = await Catalog.find(query)
+      .sort({ "product.upVotes": -1 })
+      .toArray();
+    console.log("result ->>>>>>>", result);
+    return Catalog.find(query);
   }
 }
